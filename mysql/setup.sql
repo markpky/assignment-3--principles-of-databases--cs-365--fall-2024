2a85cb125b21681292df8c084691c9781abedc3e
markpky <amar.singh.eshri@gmail.com>
2024-11-15 18:33:38 -0500

    Database Setup From Assignment 2
    Took code from assignment 2 for the setup of the database. Changed primary key for users and SHA2 to use 256.

---
+++ mysql/setup.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS 'student_passwords';
+CREATE DATABASE 'student_passwords';
+USE 'student_passwords';
+
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = UNHEX(SHA2('my secret passphrase', 256));
+SET @init_vector = RANDOM_BYTES(16);
+
+CREATE TABLE IF NOT EXISTS passwords.people (
+  personID smallint(5) NOT NULL,
+  firstName varchar(50) NOT NULL,
+  lastName varchar(50) NOT NULL,
+  email varchar(320) NOT NULL,
+  comment varchar(60) NULL,
+  CONSTRAINT people_PK PRIMARY KEY (personID)
+);
+
+CREATE TABLE IF NOT EXISTS passwords.websites (
+  websiteID smallint(5) NOT NULL,
+  name varchar(50) NOT NULL,
+  url varchar(60) NOT NULL,
+  comment varchar(60) DEFAULT NULL,
+  CONSTRAINT websites_PK PRIMARY KEY (websiteID),
+  UNIQUE KEY websites_unique (url)
+);
+
+CREATE TABLE IF NOT EXISTS passwords.users (
+  personID smallint(5) NOT NULL,
+  websiteID smallint(5) NOT NULL,
+  username varchar(100) NOT NULL,
+  password varbinary(512) NOT NULL,
+  timestamp datetime NOT NULL,
+  comment varchar(60) DEFAULT NULL,
+  PRIMARY KEY (username, websiteID),
+  CONSTRAINT users_websites_FK FOREIGN KEY (websiteID) REFERENCES websites (websiteID) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT users_people_FK FOREIGN KEY (personID) REFERENCES people (personID) ON DELETE CASCADE ON UPDATE CASCADE
+);
