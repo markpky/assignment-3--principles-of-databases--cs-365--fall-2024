fc9dde7f8ed56218a79028125b99747600b1f26a
markpky <amar.singh.eshri@gmail.com>
2024-11-18 17:21:00 -0500

    Fixed Some Pathname Bugs
    Forgot to change some pathname things from copying code from example repo. Thx for the btw. Greg appreciates it!

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submitted'])) {
-    echo "<p>You submitted the form. Hurray! Click <a href=\"index.html\">here</a> to go back.</p>";
+    echo "<p>You submitted the form. Hurray! Click <a href=\"../index.php\">here</a> to go back.</p>";
 } else {
-    echo "<p>The CREATE form from index.php was not submitted. Click <a href=\"index.html\">here</a> to go back.</p>";
+    echo "<p>The CREATE form from index.php was not submitted. Click <a href=\"../index.php\">here</a> to go back.</p>";
 }
