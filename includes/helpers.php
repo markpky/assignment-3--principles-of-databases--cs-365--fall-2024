eece8b1b2fff36190558a9f5b65baa1d905dc6b9
markpky <amar.singh.eshri@gmail.com>
2024-11-18 17:28:01 -0500

    CREATE Form Flavor Text
    Just adding some text to make sure this if structure is working. Changed some spacing stuff and other text popups as well.

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,6 +1,9 @@
 <?php
-if(isset($_POST['submitted'])) {
+if (isset($_POST['submitted'])) {
+    if ($_POST['submitted'] == "CREATE") {
+        echo "<p>Looks like you want to create a user there big guy...<p>";
+    }
     echo "<p>You submitted the form. Hurray! Click <a href=\"../index.php\">here</a> to go back.</p>";
 } else {
-    echo "<p>The CREATE form from index.php was not submitted. Click <a href=\"../index.php\">here</a> to go back.</p>";
+    echo "<p>One of the forms from index.php was not submitted. Click <a href=\"../index.php\">here</a> to go back.</p>";
 }
